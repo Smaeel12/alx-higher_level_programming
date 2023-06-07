56ec0e1b332abacaaf2b02dd547b78553e0a15a5
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-07 02:36:33 +0000

    12. Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            s = "FizzBuzz"
+            print("{} ".format(s), end="")
+        elif i % 3 == 0:
+            s = "Fizz"
+            print("{} ".format(s), end="")
+        elif i % 5 == 0:
+            s = "Buzz"
+            print("{} ".format(s), end="")
+        else:
+            print("{} ".format(i), end="")
