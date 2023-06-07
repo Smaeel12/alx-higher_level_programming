a0a0164273bac3225f12747bd0521cbd06f027b7
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-07 00:25:30 +0000

    4. Hexadecimal printing

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 if number < 0:
     last_digit = -(-number % 10)
     print(f"the last digit of {number} is\
-            {last_digit} and is less than 6 and not 0")
+ {last_digit} and is less than 6 and not 0")
 else:
     last_digit = number % 10
     if last_digit > 5:
