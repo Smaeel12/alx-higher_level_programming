2ee82e03a847b690b4a7639d7342c3078b60dbb3
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-09 01:05:31 +0000

    3. Infinite addition

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 from sys import argv
-num_args = len(argv)
-if num_args == 1:
-    str = "."
-if num_args == 2:
-    str = " argument:"
-else:
-    str = " arguments:"
+if __name__ == "__main__":
+    num_args = len(argv)
+    print(num_args)
+    if num_args == 1:
+        str = " arguments."
+    elif num_args == 2:
+        str = " argument:"
+    else:
+        str = " arguments:"
 
-print(f"{num_args - 1:d}{str:s}")
-for i in range(1, num_args):
-    print(f"{i:d}: {argv[i]:s}")
+    print(f"{num_args - 1:d}{str:s}")
+    for i in range(1, num_args):
+        print(f"{i:d}: {argv[i]:s}")
