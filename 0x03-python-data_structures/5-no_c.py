dfa3835ccbdc2ce84a38a9b30c94ccc2fc024224
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-12 16:14:17 +0000

    Correction of "3. Print a list of integers... in reverse!"

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    i = 0
-    List = list(my_string)
-    if 'c' in List:
-        List.remove('c')
-    if 'C' in List:
-        List.remove('C')
-    my_string = "".join(List)
-    return my_string
+    if my_string:
+        List = list(my_string)
+        if 'c' in List:
+            List.remove('c')
+        if 'C' in List:
+            List.remove('C')
+        my_string = "".join(List)
+        return my_string
