7aece26b7f3566d35946ca5ab173bb46238d9565
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-12 18:33:53 +0000

    9. Find the max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        MAX = 0
+        for idx in my_list:
+            if idx > MAX:
+                MAX = idx
+        return MAX
