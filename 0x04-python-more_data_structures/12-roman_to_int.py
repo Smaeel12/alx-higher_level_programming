64295a3eb57af21c91f85128568fff26f49bab97
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-17 02:48:49 +0000

    Correction of "12. Roman to Integer"

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    value_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000
-                  }
-    value_list = []
-    integer = 0
-    for char in roman_string:
-        for key in value_dict:
-            if char == key:
-                value_list.append(value_dict[key])
-    for value in value_list:
-        if integer < value:
-            integer = value - integer
-        else:
-            integer += value
-    return integer
+    if roman_string and type(roman_string) == str:
+        value_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000
+                      }
+        value_list = []
+        integer = 0
+        for char in roman_string:
+            for key in value_dict:
+                if char == key:
+                    value_list.append(value_dict[key])
+        for value in value_list:
+            if integer < value:
+                integer = value - integer
+            else:
+                integer += value
+        return integer
+    return 0
