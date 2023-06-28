dd2dae933f03578302633aef997032fd7b34ffb5
Smaeel12 <ismailboubkri2002@gmail.com>
2023-06-28 00:20:53 +0000

    Correction of "0. My first square"

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""this is an empty class Square that defines a square"""
+"""empty class Square"""
 class Square:
     """this is an empty class Square that defines a square"""
     pass
