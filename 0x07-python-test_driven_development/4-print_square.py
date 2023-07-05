a6203968fcb27253fef8c8908c815e9776f9df5e
Smaeel12 <ismailboubkri2002@gmail.com>
2023-07-05 01:20:51 +0000

    0x07. Python - Test-driven development

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    module contains a function that prints a square by the character #.
+"""
+
+
+def print_square(size):
+    """
+    Prints a square with character #.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)
