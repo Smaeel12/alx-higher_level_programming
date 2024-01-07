58b1a1ca78e07ca56689e48c3bc3ebc54034b205
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-07 21:52:40 +0000

    Correction of "2. Replace element"

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != [[]]:
-        for alist in [[c for c in r] for r in matrix]:
-            for n in alist[:-1]:
-                print('{}'.format(n), end=' ')
-            print('{}'.format(n + 1))
-    else:
+    for alist in matrix:
+        for n in alist:
+            print(n, end='')
+            if n != alist[-1]:
+                print(end=' ')
         print()
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence:
-        lenght = len(sentence)
-        if lenght > 0:
-            first_c = sentence[0]
+    lenght = len(sentence)
+    if lenght > 0:
+        first_c = sentence[0]
         return (lenght, first_c)
-    return None
+    return (0, None)
