8611a34bf6b46afa4b66e1660f7d6eb5565b3519
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-07 21:56:41 +0000

    Correction of "6. Lists of lists = Matrix"

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for alist in matrix:
         for n in alist:
-            print(n, end='')
+            print('{}'.format(n), end='')
             if n != alist[-1]:
                 print(end=' ')
         print()
