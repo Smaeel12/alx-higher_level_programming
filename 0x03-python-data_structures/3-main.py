7952c0c3bf4392f9d46ca8c4383dd4698ecefab7
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-08 17:23:09 +0000

    Correction of task 3

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
 
-my_list = [10, 13, 90, 2, 40]
-print_reversed_list_integer(my_list)
+print_reversed_list_integer()
