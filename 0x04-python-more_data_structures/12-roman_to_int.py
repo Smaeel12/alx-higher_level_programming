a768810039f1dcb47ac003085f9a94a87f991d84
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-10 21:36:15 +0000

    Correction of "12. Roman to Integer"

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,12 +3,15 @@ def roman_to_int(roman_string):
     if roman_string is not None and isinstance(roman_string, str):
         number = 0
         roman_dict = {
-                'I': 1, 'V': 5, 'X': 10,
-                'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        for c in roman_string:
-            if number < roman_dict[c]:
-                number = roman_dict[c] - number
+                'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
+        prev_value = 0
+        for c in roman_string[::-1]:
+            current_value = roman_dict[c]
+            if current_value >= prev_value:
+                number += current_value
             else:
-                number += roman_dict[c]
+                number -= current_value
+            prev_value = current_value
         return number
     return None
