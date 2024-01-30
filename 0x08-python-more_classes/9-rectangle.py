1631738c1438a10111aacbb1e562a3a866d8796d
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-30 17:22:23 +0000

    Correction of "9. A square is a rectangle"

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -69,11 +69,21 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
     @height.setter
     def height(self, value):
-        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
 
     def area(self):
         """ Public instance method that returns the rectangle area
