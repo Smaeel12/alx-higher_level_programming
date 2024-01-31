c0e890713d0084b93d15e4f7c312179090ee6b06
Smaeel12 <ismailboubkri2002@gmail.com>
2024-01-31 20:05:59 +0000

    Correction of "1. Divide a matrix"

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -27,27 +27,27 @@ def matrix_divided(matrix, div):
     elif div == 0:
         raise ZeroDivisionError("division by zero")
     elif isinstance(matrix, list) and type(div) in (int, float) and div != 0:
-        new_matrix = matrix.copy()
+        new_matrix = [alist.copy() for alist in matrix]
         size = -1
         for elist in new_matrix:
             if (size == -1):
                 size = len(elist)
             else:
                 if size != len(elist):
-                    raise TypeError("Each row of the matrix must have\
-                            the same size")
+                    raise TypeError("Each row of the matrix must have \
+the same size")
             if isinstance(elist, list):
                 for idx, num in enumerate(elist):
                     if type(num) in (int, float) and num == num\
                             and abs(num) <= 1.7976931348623158e+308:
                         elist[idx] = round(num / div, 2)
                     else:
-                        raise TypeError("matrix must be a matrix\
-                                (list of lists) of integers/floats")
+                        raise TypeError("matrix must be a matrix \
+(list of lists) of integers/floats")
             else:
-                raise TypeError("matrix must be a matrix (list of lists)\
-                        of integers/floats")
+                raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
     else:
-        raise TypeError("matrix must be a matrix (list of lists)\
-                of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
     return new_matrix
