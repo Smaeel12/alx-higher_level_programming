293159ebbf804af3c12faa1b401436fb70d36442
Smaeel12 <ismailboubkri2002@gmail.com>
2024-04-09 02:45:24 +0000

    Correction of "7. I love C"

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const numOccurrences = parseInt(process.argv[2]);
-if (!isNaN(numOccurrences) && numOccurrences > 0) {
-  for (let i = 0; i < numOccurrences; i++) {
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
   }
 } else {
