db298bf8ce279b082da1c2fb4ef285c6f7f5f745
Smaeel12 <ismailboubkri2002@gmail.com>
2024-04-14 00:44:13 +0000

    Correction of "4. Cities by states"

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
                            user=argv[1], passwd=argv[2],
                            db=argv[3], charset='utf8')
     cur = conn.cursor()
-    query = "SELECT id, state_id, name FROM cities ORDER BY cities.id ASC;"
+    query = "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN\
+            states ON cities.state_id = states.id ORDER BY cities.id ASC;"
     params = None
     cur.execute(query, params)
     query_rows = cur.fetchall()
