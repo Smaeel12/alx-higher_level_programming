453ce65bf6362ce9fad1e8f64f836ad98c678dbc
Smaeel12 <ismailboubkri2002@gmail.com>
2024-05-21 21:18:08 +0000

    Correction 456

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,6 @@
 
 const request = require('request');
 const apiUrl = process.argv[2];
-const characterId = 18;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
@@ -10,7 +9,7 @@ request(apiUrl, (error, response, body) => {
   } else {
     const films = JSON.parse(body).results;
     const count = films.reduce((acc, film) => {
-      const characterPresent = film.characters.some(characterUrl => characterUrl.includes(`/18/`));
+      const characterPresent = film.characters.some(characterUrl => characterUrl.includes('/18/'));
       return acc + (characterPresent ? 1 : 0);
     }, 0);
     console.log(count);
