2bbca51c4cda3ab885c0dbc6b26f0213acc3c9f9
Smaeel12 <ismailboubkri2002@gmail.com>
2024-05-21 21:24:34 +0000

    Correction of "8. Right order"

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,32 +2,40 @@
 
 const request = require('request');
 
+// Get the movie ID from the command line arguments
 const movieId = process.argv[2];
+
+// Construct the API URL for the specified movie ID
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+// Make a GET request to the Star Wars API endpoint for the specified movie ID
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
+    // Parse the response body
     const film = JSON.parse(body);
+    // Extract the characters' URLs in the same order as they appear in the /films/ response
     const charactersUrls = film.characters;
+
+    // Function to print characters' names in the correct order
     printCharactersInOrder(charactersUrls);
   }
 });
 
-function printCharactersInOrder (charactersUrls) {
-  let count = 0;
+// Function to print characters' names in the correct order
+function printCharactersInOrder(charactersUrls) {
+  // Iterate over the character URLs and make individual requests to get each character's details
   charactersUrls.forEach(characterUrl => {
+    // Make a GET request to the character URL
     request(characterUrl, (err, resp, characterBody) => {
       if (err) {
         console.error(err);
       } else {
+        // Parse the response body to extract character details
         const character = JSON.parse(characterBody);
+        // Print the character's name
         console.log(character.name);
-        count++;
-        if (count === charactersUrls.length) {
-          console.log('All characters printed.');
-        }
       }
     });
   });
