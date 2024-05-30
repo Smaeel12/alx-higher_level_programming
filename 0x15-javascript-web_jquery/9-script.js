3efda656686ec7e374580e94c572ee03c7fd24e3
Smaeel12 <ismailboubkri2002@gmail.com>
2024-05-30 20:53:48 +0000

    New ADD

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,10 @@
-const $ = window.$;
-const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
-
-$.getJSON(url, function (body) {
-  const hello = body.hello;
-  $('DIV#hello').text(hello);
+#!/usr/bin/node
+/* A script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr
+and displays the value of hello from that fetch in the HTML tag DIV#hello. */
+/* global $ */
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+$(document).ready(function () {
+  $.get(url, function (data) {
+    $('#hello').append(data.hello);
+  });
 });
