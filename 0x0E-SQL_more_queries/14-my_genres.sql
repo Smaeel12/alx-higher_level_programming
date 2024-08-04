eb3fa7d34944c46bb0b59f4ea4432c1f118142be
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-04 02:21:02 +0000

    Correction of "14. My genres"

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ FROM tv_show_genres
 INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
 ;
