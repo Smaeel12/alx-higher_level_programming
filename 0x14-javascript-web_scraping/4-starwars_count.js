d47872793b43f96033fabada0fa06bd48f3614c0
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-21 02:50:11 +0100

    404

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,14 +1,17 @@
 #!/usr/bin/node
-// makes get request for SW movie id
+/* A script that calculates the number of occurences
+of the character Wedge Antilles that has the ID 8. */
 const request = require('request');
-const find = '/18/';
-request(process.argv[2], function (error, response, body) {
-  if (error) throw new Error(error);
-  let num = 0;
-  for (const film of JSON.parse(body).results) {
-    for (const character of film.characters) {
-      num += (character.endsWith(find) ? 1 : 0);
+const url = process.argv.slice(2);
+let counter = 0;
+let result;
+const target = '/18/';
+request(url[0], (error, response, body) => {
+  if (error) { return; }
+  for (result of JSON.parse(body).results) {
+    for (const line of result.characters) {
+      if (line.endsWith(target)) { counter += 1; }
     }
   }
-  console.log(num);
+  console.log(counter);
 });
