c7098bc605a46ddb67d553883b34a1dc455493fe
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-21 02:43:49 +0100

    404

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ request.get(process.argv[2], function (err, response, body) {
   err && console.log(err);
   let count = 0;
   for (const film of JSON.parse(body).results) {
-    if (film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-      count++;
-    }
+    film.characters.forEach(element => {
+      if (element.includes('/18/')) count++;
+    });
   }
   console.log(count);
 });
