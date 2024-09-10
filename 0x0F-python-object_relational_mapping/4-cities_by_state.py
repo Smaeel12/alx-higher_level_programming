6f3a9ff94c03c66ddb400b2aa98294be86a1353e
Smaeel12 <ismailboubkri2002@gmail.com>
2024-09-10 20:33:05 +0100

    new update

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-
+""" script that lists all states with a name starting with N (upper N) from
+the database hbtn_0e_0_usa
+"""
 from MySQLdb import connect
 from sys import argv
 
 conn = connect(user=argv[1], passwd=argv[2], db=argv[3])
 cur = conn.cursor()
-cur.execute(
-    "SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id=states.id ORDER BY cities.id")
+cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN\
+            states ON cities.state_id=states.id ORDER BY cities.id")
 print(*cur.fetchall(), sep='\n')
 
 
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-
+""" script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 from MySQLdb import connect
 from sys import argv
 
 conn = connect(user=argv[1], passwd=argv[2], db=argv[3])
 cur = conn.cursor()
 cur.execute(
-    "SELECT cities.name FROM cities JOIN states ON cities.state_id=states.id WHERE states.name=%s ORDER BY cities.id", (argv[4],))
+    "SELECT cities.name FROM cities JOIN states ON cities.state_id=states.id\
+        WHERE states.name=%s ORDER BY cities.id", (argv[4],))
 
 print(', '.join([r[0] for r in cur.fetchall()]))
 
